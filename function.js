3043f6ae317574d13a94a23bcba75e974e663bc9
santhoshkumar <its.santhoshkumar@outlook.com>
2023-04-14 19:43:22 +0530

    fix; changes in the function expression

--- function.js
+++ function.js
@@ -18,3 +18,25 @@ const funcExpresWithDeclaration = (a, b) => a * b;
 (function () {
     console.log('this is self invoking function because')
 })();
+
+
+function sum(...args) {
+    let sum = 0;
+    for (let arg of args) sum += arg;
+    return sum;
+}
+
+let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
+
+x = findMax(1, 123, 500, 115, 44, 88);
+
+function findMax() {
+    let max = -Infinity;
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > max) {
+            max = arguments[i];
+        }
+    }
+    return max;
+}
+console.log(x);
